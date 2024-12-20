225909fa0201ed5442c64d58440479183674aefb
Vaibhav Lande <72780719+VaibhavLande@users.noreply.github.com>
2024-12-20 15:25:36 +0530

    Microservice Communication Using kafka
    Microservice Communication Using kafka

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using OrderService.Kafka;
+
+namespace OrderService.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class OrderController : ControllerBase
+	{
+		private readonly IKafkaProducer _kafkaProducer;
+		public OrderController(IKafkaProducer kafkaProducer) {
+			_kafkaProducer = kafkaProducer;
+		}
+
+
+		[HttpPost]
+		public async Task<ActionResult<string>> PostOrder(string orderName, string orderId)
+		{
+			try
+			{
+				await _kafkaProducer.ProduceAsync("order-topic", new Message<string, string>
+				{
+					Key = orderId,
+					Value = orderName
+				});
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+
+			return orderName;
+		}
+	}
+}
---
+++ ProductService/Kafka/KafkaConsumer.cs
@@ -0,0 +1,46 @@
+﻿using Confluent.Kafka;
+
+namespace ProductService.Kafka
+{
+	public class KafkaConsumer : BackgroundService
+	{
+		protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+
+			return Task.Run(() =>
+			{
+				_ = ConsumeAsync("order-topic", stoppingToken);
+			}, stoppingToken);
+		}
+
+		public async Task ConsumeAsync(string topic, CancellationToken stoppingToken)
+		{
+			try
+			{
+				var config = new ConsumerConfig
+				{
+					GroupId = "order-group",
+					BootstrapServers = "localhost:9092",
+					AutoOffsetReset = AutoOffsetReset.Earliest
+				};
+				using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+				consumer.Subscribe(topic);
+
+				while (!stoppingToken.IsCancellationRequested)
+				{
+					var consumeResult = consumer.Consume(stoppingToken);
+
+					var order = consumeResult;
+
+				}
+				consumer.Close();
+			}
+			catch (Exception ex )
+			{
+				Console.WriteLine(ex.Message);
+			}
+			
+		}
+	}
+}
